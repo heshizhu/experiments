e5b3393b932a08c6cbcc343b0166789cd086c852
heshizhu <shizhuhe@gmail.com>
2015-03-06 11:11:04 +0800

    commit openqa

---
+++ openqa/src/main/java/org/nlpr/cip/openqa/JavaMain.java
@@ -0,0 +1,12 @@
+package org.nlpr.cip.openqa;
+
+import org.apache.log4j.Logger;
+
+/**
+ * User: hesz
+ * Date: 2015-3-6
+ * Time: 11:09
+ */
+public class JavaMain {
+    private static Logger log = Logger.getLogger(JavaMain.class);
+}
